12afcfb36bbbbd4f0f17e5ab8d577e6fb5785ffd
KIMSEONGRAN <doll950904@gmail.com>
2023-01-09 21:12:39 +0900

    :sparkle: loginInput Dto 생성

---
+++ backend/src/apis/auth/dto/login.input.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MinLength } from 'class-validator';
+
+export class AuthInput {
+  @IsString()
+  @MinLength(1, {
+    message: 'Your id is wrong! please check your id.',
+  })
+  @ApiProperty({
+    example: 'user123',
+  })
+  id: string;
+
+  @IsString()
+  @MinLength(1, {
+    message: 'Your password is wrong! please check your password',
+  })
+  @ApiProperty({
+    example: '12345!@fd',
+  })
+  password: string;
+}
