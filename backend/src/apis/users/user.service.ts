3446ddc7163f7d5b14664b6e25d81ca47eb8cbac
KIMSEONGRAN <doll950904@gmail.com>
2023-01-09 21:13:17 +0900

    :sparkle: isVailedUser 추가(로그인을 위한 회원정보 조회)

--- backend/src/apis/users/user.service.ts
+++ backend/src/apis/users/user.service.ts
@@ -16,6 +16,20 @@ export class UserService {
   // /////////////////////////////////////////////////////////////////
   // utils
 
+  /** 로그인을 위한 회원정보 판별 */
+  async isVailedUser(
+    id: string, //
+  ) {
+    const user = await this.userRepository.findOne({
+      where: { id: id },
+    });
+
+    if (!user) {
+      throw new ConflictException('일치하는 id가 없습니다.');
+    }
+    return user;
+  }
+
   /** 유효한 비밀번호 check */
   async checkValidatePwd(
     pwd: string, //
@@ -36,7 +50,7 @@ export class UserService {
   /** 중복 id, nickname 체크 */
   async checkInfo(
     id: string, //
-    nickName: string,
+    nickName?: string,
   ) {
     const user = await this.userRepository.find({
       where: [
@@ -44,7 +58,8 @@ export class UserService {
         { nickName },
       ],
     });
-    if (user !== undefined) {
+
+    if (user.length !== 0) {
       throw new ConflictException('이미 있는 아이디 혹은 닉네임 입니다.');
     }
 
