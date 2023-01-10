ef1290a06f619dc597b4fe907bd19e0efd627b86
KIMSEONGRAN <doll950904@gmail.com>
2023-01-10 19:21:03 +0900

    :sparkle: refreshToken 발급 및 Header에 전달

--- backend/src/apis/auth/auth.service.ts
+++ backend/src/apis/auth/auth.service.ts
@@ -9,6 +9,7 @@ export class AuthService {
     private readonly jwtService: JwtService, //
   ) {}
 
+  /** AccessToken 발급 */
   getAccessToken(
     user: UserEntity, //
   ) {
@@ -17,5 +18,17 @@ export class AuthService {
       { secret: 'accessKey', expiresIn: '1h' },
     );
     return accessToken;
+  }
+
+  /** RefreshToken 생성 */
+  setRefreshToken(
+    user: UserEntity, //
+    res: any,
+  ) {
+    const refreshToken = this.jwtService.sign(
+      { id: user.id, nickName: user.nickName },
+      { secret: 'refreshToken', expiresIn: '2w' },
+    );
+    return res.setHeader('Set-Cookie', `refreshToken=${refreshToken}`);
   }
 }
