bc077ce7bec86e8ec5f910658934b60f2475d2fb
KIMSEONGRAN <doll950904@gmail.com>
2023-01-10 16:04:29 +0900

    :construction: AuthGuard 작업중

--- backend/src/apis/users/user.controller.ts
+++ backend/src/apis/users/user.controller.ts
@@ -1,6 +1,24 @@
 import { Response } from 'express';
-import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Query,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 
 import { UserService } from './user.service';
 import { CreateUserInput } from './dto/createUser.input';
@@ -11,6 +29,22 @@ export class UserController {
   constructor(
     private readonly userService: UserService, //
   ) {}
+
+  @ApiBearerAuth('access-token or refresh-token')
+  @UseGuards(AuthGuard('accessToken'))
+  @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
+  @ApiQuery({
+    type: CreateUserInput, //
+  })
+  @Get('/getUser')
+  async getUsers(
+    @Req() req: Express.Request,
+    @Query() user_id: CreateUserInput,
+  ) {
+    console.log('req', req);
+    console.log(user_id);
+    return '성공!!';
+  }
 
   // ========================================================= //
   /** 회원가입하기 */
