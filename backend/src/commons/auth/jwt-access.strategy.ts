f73f7bb3084cd24e0be4ad1d34e337da843d7e0b
KIMSEONGRAN <doll950904@gmail.com>
2023-01-10 16:07:09 +0900

    :sparkle: JWT인가 과정 작성

--- backend/src/commons/auth/jwt-access.strategy.ts
+++ backend/src/commons/auth/jwt-access.strategy.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { Strategy, ExtractJwt } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+
+import { IPayloadSub } from '../interfaces/payload.interface';
 
 @Injectable()
 export class JwtAccessStrategy extends PassportStrategy(
   Strategy, //
-  'access',
+  'accessToken',
 ) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.forAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: 'accessKey',
     });
   }
 
+  /** 검증 성공 시 실행 */
   async validate(
-    payload: any, //
-  ) {}
+    payload: IPayloadSub, //
+  ) {
+    return {
+      id: payload.id,
+      nickname: payload.nickName,
+    };
+  }
 }
