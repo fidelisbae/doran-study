5216cab9ce37ed39a01b721cd4fb908c474b5444
KIMSEONGRAN <doll950904@gmail.com>
2023-01-17 22:32:46 +0900

    :adhesive_bandage: CleanUp line

--- backend/src/apis/auth/auth.module.ts
+++ backend/src/apis/auth/auth.module.ts
@@ -4,12 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserModule } from '../users/user.module';
 import { UserEntity } from '../users/entities/user.entity';
-
 import { JwtAccessStrategy } from 'src/commons/auth/jwt-access.strategy';
+import { JwtRefreshStrategy } from 'src/commons/auth/jwt-refresh.strategy';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtRefreshStrategy } from 'src/commons/auth/jwt-refresh.strategy';
 
 @Module({
   imports: [
