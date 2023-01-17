9e5a2c760aeb9f1f0f1a4d77f513aa361b7fd1dc
KIMSEONGRAN <doll950904@gmail.com>
2023-01-17 22:34:06 +0900

    :sparkle: Add: logout service process 작성

--- backend/src/apis/auth/auth.service.ts
+++ backend/src/apis/auth/auth.service.ts
@@ -1,13 +1,57 @@
+import Redis from 'ioredis';
 import { JwtService } from '@nestjs/jwt';
-import { Injectable } from '@nestjs/common';
+import { InjectRedis } from '@liaoliaots/nestjs-redis';
+import { ConflictException, Injectable } from '@nestjs/common';
 
 import { UserEntity } from '../users/entities/user.entity';
+import { UserService } from '../users/user.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly jwtService: JwtService, //
+    private readonly userService: UserService,
+
+    @InjectRedis('refresh_token')
+    private readonly refresh_token_pool: Redis,
+
+    @InjectRedis('access_token')
+    private readonly access_token_pool: Redis,
   ) {}
+
+  /** 로그아웃 */
+  async logout(
+    userID: string,
+    accessToken: string,
+    refreshToken: string, //
+  ) {
+    let result = false;
+    const user = await this.userService.isValidUser(userID);
+
+    if (!user) {
+      throw new ConflictException('존재하지 않는 회원입니다.');
+    } else {
+      result = true;
+    }
+
+    await this.access_token_pool.set(
+      accessToken.replace('Bearer ', ''),
+      userID,
+      'EX',
+      30,
+    );
+
+    await this.refresh_token_pool.set(
+      refreshToken.replace('refreshToken=', ''),
+      userID,
+      'EX',
+      3600 * 24 * 14,
+    );
+
+    return result === true
+      ? '로그아웃 되었습니다.'
+      : '로그아웃을 할 수 없습니다.';
+  }
 
   /** AccessToken 발급 */
   getAccessToken(
@@ -15,7 +59,7 @@ export class AuthService {
   ) {
     const accessToken = this.jwtService.sign(
       { id: user.id, nickName: user.nickName },
-      { secret: 'accessKey', expiresIn: '30s' },
+      { secret: 'accessKey', expiresIn: '1h' },
     );
     return accessToken;
   }
