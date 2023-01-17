720aa14f5111c14496f39a36b88970a18326d9f0
KIMSEONGRAN <doll950904@gmail.com>
2023-01-17 22:36:31 +0900

    :fire: Delete interface IPayload

--- backend/src/commons/interfaces/payload.interface.ts
+++ backend/src/commons/interfaces/payload.interface.ts
@@ -3,14 +3,6 @@ interface __Payload {
   nickName: string;
 }
 
-export interface IPayload extends __Payload {
-  id: string;
-  access_token: string;
-  access_exp: number;
-  refresh_token: string;
-  refresh_exp: number;
-}
-
 export interface IPayloadSub extends __Payload {
   exp?: number;
   iat?: number;
