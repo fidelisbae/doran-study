22676d9e22add932aee56e8423b6fd3cb47826b5
KIMSEONGRAN <doll950904@gmail.com>
2023-02-01 23:07:25 +0900

    :sparkle: Add: 회원탈퇴

--- backend/src/apis/users/user.controller.ts
+++ backend/src/apis/users/user.controller.ts
@@ -28,6 +28,7 @@ import {
 import { UserService } from './user.service';
 import { CreateUserInput } from './dto/createUser.input';
 import { UpdateUserInput } from './dto/updateUser.input';
+import { DeleteUserInput } from './dto/deleteUser.input';
 
 @ApiTags('Users')
 @Controller('/users')
@@ -101,6 +102,36 @@ export class UserController {
     const isBool = await this.userService.updateUser(user.id, input);
     const result =
       isBool === true ? '수정이 완료되었습니다.' : '수정에 실패했습니다.';
+
+    return res.status(HttpStatus.OK).json(result);
+  }
+
+  /** 회원탈퇴 */
+  @ApiBearerAuth('access-token or refresh-token')
+  @UseGuards(AuthGuard('accessToken'))
+  @ApiOperation({
+    summary: '회원탈퇴하기',
+  })
+  @ApiBody({
+    type: DeleteUserInput, //
+  })
+  @Delete('/deleteUser')
+  async deleteUser(
+    @Body() input: DeleteUserInput, //
+    @Req() req: Express.Request,
+    @Res() res: Response,
+  ) {
+    const user = await this.userService.isValidUser(req['user'].id);
+    const isBool = await this.userService.deleteUser(
+      user.id,
+      req['body'].password,
+      input,
+    );
+
+    const result =
+      isBool === true
+        ? '회원 탈퇴되었습니다. 이용해주셔서 감사합니다.'
+        : '실패했습니다. 다시 시도해주세요.';
 
     return res.status(HttpStatus.OK).json(result);
   }
