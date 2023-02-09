6e00625c0af36111f98b94afecf002ab3879710d
KIMSEONGRAN <doll950904@gmail.com>
2023-02-09 17:24:39 +0900

    :sparkle: Add: ChatService 및 Entity 추가

--- backend/src/commons/socket/socket.module.ts
+++ backend/src/commons/socket/socket.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ChatModule } from 'src/apis/chat/chat.module';
+import { ChatEntity } from 'src/apis/chat/entities/chat.entity';
+import { ChatService } from 'src/apis/chat/chat.service';
 
 import { SocketGateway } from './socket.gateway';
 
 @Module({
-  imports: [JwtModule.register({})], //
-  providers: [SocketGateway],
+  imports: [
+    JwtModule.register({}), //
+    TypeOrmModule.forFeature([
+      ChatEntity, //
+    ]),
+    ChatModule,
+  ], //
+  providers: [SocketGateway, ChatService],
   exports: [SocketGateway],
 })
 export class SocketModule {}
