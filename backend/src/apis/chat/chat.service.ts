4741a7a6c0f7358078358e570fc5c284d92228af
KIMSEONGRAN <doll950904@gmail.com>
2023-02-09 17:22:50 +0900

    :sparkle: Add: 채팅방 정보 조회 및 멤버 변경 API 추가

--- backend/src/apis/chat/chat.service.ts
+++ backend/src/apis/chat/chat.service.ts
@@ -1,4 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { ERROR } from 'src/commons/utils/error.enum';
+
+import { ChatDTO } from './dto/chat.dto';
+import { ChatEntity } from './entities/chat.entity';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(
+    @InjectRepository(ChatEntity)
+    private readonly chatRepository: Repository<ChatEntity>,
+  ) {}
+
+  /** 방장 조회 */
+  async getHost(
+    input: ChatDTO, //
+  ) {
+    const result = await this.chatRepository.findOne({
+      where: { host: input.host, roomName: input.roomName },
+    });
+
+    if (!result) {
+      throw new ConflictException(ERROR.CAN_NOT_FIND_CHAT);
+    }
+
+    return result;
+  }
+
+  /** 방 조회 */
+  async getRoom(
+    roomName: string, //
+  ) {
+    const result = await this.chatRepository.findOne({
+      where: { roomName: roomName },
+    });
+
+    if (!result) {
+      throw new ConflictException(ERROR.CAN_NOT_FIND_CHAT);
+    }
+
+    return result;
+  }
+
+  /** 방 생성 시 정보 저장 */
+  async saveRoomInfo(
+    host: string,
+    roomName: string,
+    users: string[], //
+  ) {
+    await this.chatRepository.save({
+      host,
+      roomName,
+      users,
+    });
+  }
+
+  /** 방 정보 업데이트 */
+  async updateRoomInfo(
+    users: Array<string>,
+    roomName?: string, //
+    host?: string,
+  ) {
+    const room = await this.chatRepository.findOne({
+      where: { roomName: roomName },
+    });
+
+    if (!room) {
+      throw new ConflictException(ERROR.CAN_NOT_FIND_CHAT);
+    }
+
+    await this.chatRepository.update(
+      { host: room.host, id: room.id, roomName: room.roomName },
+      { users: users },
+    );
+  }
+}
