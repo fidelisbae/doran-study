5f7cfe91cb15ae56efd79a2f3197c0bbc02e60d1
KIMSEONGRAN <doll950904@gmail.com>
2023-02-24 22:45:22 +0900

    :construction: Working: RedisConfig

---
+++ backend/src/commons/configs/redis.config.ts
@@ -0,0 +1,50 @@
+import {
+  RedisModuleAsyncOptions,
+  RedisModuleOptions,
+} from '@liaoliaots/nestjs-redis';
+import { ConfigService } from '@nestjs/config';
+
+export class RedisConfig {
+  static getRedisConfig(configService: ConfigService): RedisModuleOptions {
+    return {
+      config: [
+        {
+          host: configService.get<string>('REDIS_HOST'),
+          port: configService.get<number>('REDIS_DOCKER_PORT'),
+          namespace: 'access_token',
+          db: 1,
+        },
+        {
+          host: configService.get<string>('REDIS_HOST'),
+          port: configService.get<number>('REDIS_DOCKER_PORT'),
+          namespace: 'refresh_token',
+          db: 2,
+        },
+        {
+          host: configService.get<string>('REDIS_HOST'),
+          port: configService.get<number>('REDIS_DOCKER_PORT'),
+          namespace: 'chat-room',
+          db: 3,
+        },
+        {
+          host: configService.get<string>('REDIS_HOST'),
+          port: configService.get<number>('REDIS_DOCKER_PORT'),
+          namespace: 'rooms',
+          db: 4,
+        },
+        {
+          host: configService.get<string>('REDIS_HOST'),
+          port: configService.get<number>('REDIS_DOCKER_PORT'),
+          namespace: 'Banned-users',
+          db: 5,
+        },
+        {
+          host: configService.get<string>('REDIS_HOST'),
+          port: configService.get<number>('REDIS_DOCKER_PORT'),
+          namespace: 'userList-InTheRoom',
+          db: 6,
+        },
+      ],
+    };
+  }
+}
