ef8436ddfede545f93497c37dac531b8aad4ebd0
KIMSEONGRAN <doll950904@gmail.com>
2023-02-24 22:08:55 +0900

    :recycle: Recycle: authController

--- backend/src/apis/auth/auth.controller.ts
+++ backend/src/apis/auth/auth.controller.ts
@@ -34,53 +34,27 @@ export class AuthController {
 
   //////////////////////////////////////////////////////////
   /** 로그인하기 */
-  @ApiOperation({
-    summary: '로그인하기',
-  })
-  @ApiBody({
-    type: AuthInput, //
-  })
+  @ApiBody({ type: AuthInput })
   @Post('/login')
   async login(
     @Body() input: AuthInput, //
     @Res() res: Response,
   ) {
-    const user = await this.userService.isValidUser(input.id);
-    const isAuthenticated = await bcrypt.compare(input.password, user.password);
-
-    if (!isAuthenticated) {
-      throw new UnauthorizedException(ERROR.INVALID_USER_PASSWORD);
-    }
-
-    this.authService.setRefreshToken(user, res);
-    const accessToken = this.authService.getAccessToken(user);
-
-    return res.status(201).json({ accessToken: accessToken });
+    const result = await this.authService.login(input.id, input.password, res);
+    return res.status(201).json({ accessToken: result });
   }
 
   /** 로그아웃 */
   @ApiBearerAuth('access-token or refresh-token')
   @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
   @UseGuards(AuthGuard('accessToken'))
-  @ApiOperation({
-    summary: '로그아웃하기',
-  })
   @Post('/logout')
   async logout(
     @Req() req: Express.Request,
     @Res() res: Response, //
   ) {
-    const user = req['user'];
-    const header = req['headers'];
-    const refreshToken = header.cookie.replace('refreshToken=', '');
-    const accessToken = header.authorization.replace('Bearer ', '');
-
-    const result = await this.authService.logout(
-      user.id,
-      accessToken,
-      refreshToken,
-    );
-    return result === ERROR.SESSION_SUCCESS_PASSED
+    const result = await this.authService.logout(req['user'].id, req);
+    return result
       ? res.status(201).json(ERROR.SESSION_SUCCESS_PASSED)
       : res.status(404).json(ERROR.SESSION_DESTROY_FAILED);
   }
@@ -90,28 +64,12 @@ export class AuthController {
   @ApiBearerAuth('access-token or refresh-token')
   @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
   @UseGuards(AuthGuard('refreshToken'))
-  @ApiOperation({
-    summary: 'AccessToken 재발급하기',
-  })
-  @Post('/restoreAccessToken')
+  @Post()
   async restoreAccessToken(
     @Req() req: Express.Request, //
     @Res() res: Response,
   ) {
-    const userInfo = req['user'];
-    const user = await this.userService.isValidUser(userInfo.id);
-
-    const oldAccessToken = req['headers'].authorization.replace('Bearer ', '');
-    const oldRefreshToken = req['headers'].cookie.replace('refreshToken=', '');
-
-    await this.authService.setBlackList(
-      user.id,
-      oldAccessToken,
-      oldRefreshToken,
-    );
-
-    await this.authService.setRefreshToken(user, res);
-    const accessToken = this.authService.getAccessToken(user);
-    return res.status(201).json({ accessToken: accessToken });
+    const result = await this.authService.restoreAccessToken(req, res);
+    return res.status(201).json({ accessToken: result });
   }
 }
