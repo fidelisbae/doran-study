04944eebe4c286eb3160493cc92b2c656b897c2b
KIMSEONGRAN <doll950904@gmail.com>
2023-02-27 23:31:24 +0900

    :recycle: Update: Typeorm, redis config 분리

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,68 +7,14 @@ import { ChatModule } from './apis/chat/chat.module';
 import { AuthModule } from './apis/auth/auth.module';
 import { UserModule } from './apis/users/user.module';
 import { SocketModule } from './commons/socket/socket.module';
+import { redisConfigAsync } from './commons/configs/redis.config';
+import { typeOrmConfigAsync } from './commons/configs/typeorm.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: `${process.env.MYSQL_HOST}`,
-      port: Number(process.env.MYSQL_DOCKER_PORT),
-      username: process.env.MYSQL_USER_NAME,
-      password: process.env.MYSQL_ROOT_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
-      entities: [
-        __dirname + '/apis/**/*.entity.*', //
-      ],
-      synchronize: true,
-      logging: true,
-    }),
-
-    RedisModule.forRoot({
-      config: [
-        {
-          host: 'localhost',
-          namespace: 'access_token',
-          db: 1,
-          port: 26379,
-        },
-        {
-          host: 'localhost',
-          namespace: 'refresh_token',
-          db: 2,
-          port: 26379,
-        },
-        {
-          host: 'localhost',
-          namespace: 'socket_room',
-          db: 3,
-          port: 26379,
-        },
-        {
-          host: 'localhost',
-          namespace: 'rooms',
-          db: 4,
-          port: 26379,
-        },
-        {
-          host: 'localhost',
-          namespace: 'BannedUsers',
-          db: 5,
-          port: 26379,
-        },
-        {
-          host: 'localhost',
-          namespace: 'user_listInRoom',
-          db: 6,
-          port: 26379,
-        },
-      ],
-    }),
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    RedisModule.forRootAsync(redisConfigAsync),
 
     AuthModule,
     ChatModule,
